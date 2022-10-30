9a0e2d2de60ffee7ee033620196f0a190999191b
EdmundoJuarez117X <114251337+EdmundoJuarez117X@users.noreply.github.com>
2022-10-30 16:07:06 -0600

    Stripe Styles Update
    Colores a los botones de regreso al dashboard para los archivos success.php y cancel.php

--- views/stripeInscrip/public/success.php
+++ views/stripeInscrip/public/success.php
@@ -2,6 +2,8 @@
 <html>
 <head>
   <title>Gracias por su compra!</title>
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -10,7 +12,7 @@
       Tu pago se realizó con éxito! Si tienes dudas por favor envía correo:
       <a href="mailto:orders@example.com">orders@example.com</a>.
     </p>
-    <a href="../../dashboard/inicio.php">Regresar al Dashboard</a>
+    <a class="btn btn-outline-success" href="../../dashboard/inicio.php">Regresar al Dashboard</a>
   </section>
 </body>
 </html>
